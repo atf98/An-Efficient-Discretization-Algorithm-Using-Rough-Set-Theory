d5df45e4349f4aacc244dc310c70a93177fac903
Ahmad Tariq Alflahat <ahmadtariqf@gmail.com>
2022-04-04 15:09:53 +0300

    .

--- tooldev/rst.py
+++ tooldev/rst.py
@@ -6,15 +6,21 @@ class RST:
         data: pd.DataFrame = None,
         continuous_columns: list = [],
         decision_column_name: str = 'class',
+        include_continuous_col: bool = False,
     ):
-
         
         # Primary variable of RST class
-        self.data = data
-        self.data_indiscernibility = {}
-        self.reduct_combination = []
         self.continuous_columns = continuous_columns
         self.decision_column_name = decision_column_name
+
+        if include_continuous_col:
+            self.data = data
+        else:
+            self._data = data
+            self.data = self._data[set(self._data.columns).difference(self.continuous_columns)]
+        self.data_indiscernibility = {}
+        self.unique_indiscernibility = []
+        self.reduct_combination = []
         self.lower_approx = {}
         self.upper_approx = {}
         self.boundary_region = {}
@@ -22,39 +28,51 @@ class RST:
         self.U = self._list_to_set_conversion(self.data.index.values.tolist())
 
         # Config Variable
-        self.target_sets = self.get_indiscernibility([decision_column_name])
         self.target_unique = self.data[decision_column_name].unique()
+        self.target_sets = self.get_indiscernibility([decision_column_name])[0]
         self.target_sets_dict = {self.target_unique[idx]: ele for idx, ele in enumerate(self.target_sets[decision_column_name])}
 
     def get_indiscernibility(self, combination: list = []):
-
+        
         def indices(lst, item):
             return [i for i, x in enumerate(lst) if x == item]
-
+            
         if self.decision_column_name not in combination:
             combination.append(self.decision_column_name)
-            
+
+        
         selected_columns = self.data[combination].to_dict('list')
         data = list(zip(*selected_columns.values()))
 
-        self.data_indiscernibility.update({
-            self._create_names(combination): list(indices(data, x) for x in set(data) if data.count(x) > 1)
-        })
-        return self.data_indiscernibility
+        # print(combination, list(indices(data, x) for x in set(data) if data.count(x) > 1))
+        if list(indices(data, x) for x in set(data) if data.count(x) > 1) in self.unique_indiscernibility:
+            self.reduct_combination.append(self._create_names(combination))
+        else:
+            self.unique_indiscernibility.append(list(indices(data, x) for x in set(data) if data.count(x) > 1))
+
+            self.data_indiscernibility.update({
+                self._create_names(combination): list(indices(data, x) for x in set(data) if data.count(x) > 1)
+            })
+
+        return [self.data_indiscernibility, self.reduct_combination]
 
     def set_main_variable(self, combination: list = []):
+        combination.append(self.decision_column_name)
         _c_n = self._create_names(combination)  # combination name
+        
+        if _c_n not in self.data_indiscernibility:
+            self.get_indiscernibility(combination)
+        if _c_n in self.reduct_combination:
+            return [{},{},{},{}]
+
         combination_upper = {}
         combination_lower = {}
 
         for target, set in self.target_sets_dict.items():
-            if _c_n not in self.data_indiscernibility:
-                self.get_indiscernibility(combination)
-
             c = combination.copy()
             c.append(str(target))
             __c_n = self._create_names(c)
-
+            
             self.lower_approx.update({__c_n: []})
             self.upper_approx.update({__c_n: []})
             self.boundary_region.update({__c_n: []})
@@ -73,14 +91,10 @@ class RST:
             upper = self._list_to_set_conversion(self.upper_approx[__c_n])
             lower = self._list_to_set_conversion(self.lower_approx[__c_n])
             
-
-            self.boundary_region[__c_n].extend(
-                upper - lower
-            )
-
-            self.outside_region[__c_n].extend(
-                self.U - upper
-            )
+            # print(upper, lower)
+            self.boundary_region[__c_n].extend(upper - lower)
+            self.outside_region[__c_n].extend(self.U - upper)
+            # print(self.boundary_region)
 
         return [
             combination_upper,
@@ -99,7 +113,7 @@ class RST:
         ]
 
     def _create_names(self, e) -> str:
-        return '_'.join(e)
+        return '|'.join(e)
 
     def _list_to_set_conversion(self, l):
         se = set()
